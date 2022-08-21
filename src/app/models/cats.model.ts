2bf665384d8a7fb570f15ae384564d571802f28e
Vitaliy_Prihotskiy <vitaliy.pryhotsky@gmail.com>
2022-08-21 19:38:18 +0300

    pagination and some more fixes

--- src/app/models/cats.model.ts
+++ src/app/models/cats.model.ts
@@ -1,20 +1,20 @@
 export interface CatFromAPI {
-  breeds: {
-    0: {
+  breeds: [
+    {
       description: string;
       name: string;
       temperament: string;
-    };
-  };
+    }
+  ];
+
   height: number;
   id: string;
   url: string;
   width: number;
 }
 export interface Cat {
-  breedName:string;
-  breedDescription:string;
-  breedTemperament:string;
+  breedName: string;
+  breedDescription: string;
+  breedTemperament: string;
   url: string;
 }
-
